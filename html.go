0021ae21458e27e62dc10e6bf2a88f2b8b52cfa9
qemm <qemm@qemm.org>
2016-11-18 16:36:38 +0100

    version 90

--- html.go
+++ html.go
@@ -21,6 +21,7 @@ const (
 	html = `<!doctype html>
 <html>
 <head>
+asdasd
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
 aa<!-- Compiled and minified JavaScript -->
