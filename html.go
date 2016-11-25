60adb5379cbed9ead746011ae5e6bab2cb6b5f12
qemm <qemm@qemm.org>
2016-11-25 20:42:57 +0100

    html1

--- html.go
+++ html.go
@@ -23,7 +23,7 @@ const (
 <head>
 asdasd
 <!-- Compiled and minified CSS -->
-
+HOOLLAAAA
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
 aa<!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
